f71c5334f119af003a29464c7b1c083c53e55df7
starekrow <starekrow@metalcoder.com>
2017-12-11 15:01:39 -0800

    Fix access

--- src/CryptoCoreFailed.php
+++ src/CryptoCoreFailed.php
@@ -17,43 +17,43 @@ use Exception;
 class CryptoCoreFailed 
     implements CryptoCore
 {
-    protected function hash( $alg, $data )
+    public function hash( $alg, $data )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function hmac( $alg, $key, $data )
+    public function hmac( $alg, $key, $data )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function hkdf( $alg, $ikm, $len, $salt = "", $info = "" )
+    public function hkdf( $alg, $ikm, $len, $salt = "", $info = "" )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function encrypt( $alg, $key, $iv, $data )
+    public function encrypt( $alg, $key, $iv, $data )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function decrypt( $alg, $key, $iv, $data )
+    public function decrypt( $alg, $key, $iv, $data )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function hashcmp( $h1, $h2 )
+    public function hashcmp( $h1, $h2 )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function random( $count )
+    public function random( $count )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function ivlen( $alg )
+    public function ivlen( $alg )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function keylen( $alg )
+    public function keylen( $alg )
     {
         throw new Exception( "No usable crypto core" );
     }
-    protected function algolist()
+    public function algolist()
     {
         throw new Exception( "No usable crypto core" );
     }
