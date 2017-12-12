7ac12f75522d78cd7b3f65c0d1a5edc591db6949
starekrow <starekrow@metalcoder.com>
2017-12-11 17:54:50 -0800

    Update README with Crypto information; also, more tests

--- tests/CryptoTest.php
+++ tests/CryptoTest.php
@@ -36,6 +36,43 @@ class CryptoTest extends TestCase
         $this->assertNotEquals( Crypto::init( "openssl" ), false );
     }
 
+    public function testOpenssl_AES128CBC()
+    {
+        Crypto::init( "openssl" );
+        $key = hex2bin( "d7397beb86bc85d590fc0b8c53c6188f" );
+        $iv = hex2bin( "b0aefb01e733b0e2baf44b4ab77b5870" );
+        $msg = hex2bin( "999689c32050125dda7250c9c9aae0ec" );
+        $ct = Crypto::encrypt( "AES-128-CBC", $key, $iv, $msg );
+        $this->assertEquals( 
+            "2af8f5de48e24ae249696b8685b4b57170626116e4a159922dc408ca31ee5ee9", 
+            bin2hex( $ct ) );
+    }
+
+    public function testOpenssl_AES128CFB()
+    {
+        Crypto::init( "openssl" );
+        $key = hex2bin( "d7397beb86bc85d590fc0b8c53c6188f" );
+        $iv = hex2bin( "b0aefb01e733b0e2baf44b4ab77b5870" );
+        $msg = hex2bin( "999689c32050125dda7250c9c9aae0ec" );
+        $ct = Crypto::encrypt( "AES-128-CFB", $key, $iv, $msg );
+        $this->assertEquals( 
+            "7fec0d24d21c3edbd9abc0359b6b1c89", 
+            bin2hex( $ct ) );
+    }
+
+    public function testOpenssl_AES128ECB()
+    {
+        Crypto::init( "openssl" );
+        // one of the NIST test vectors
+        $key = hex2bin( "139a35422f1d61de3c91787fe0507afd" );
+        $iv = null;
+        $msg = hex2bin( "b9145a768b7dc489a096b546f43b231f" );
+        $ct = Crypto::encrypt( "AES-128-ECB", $key, $iv, $msg );
+        $this->assertEquals( 
+            "0da1b56ba11c1a5500e95583c0eac913a9ed2204e460199a52bea0433523f504", 
+            bin2hex( $ct ) );
+    }
+
     public function testResetToAutoloader()
     {
         $this->assertNotEquals( Crypto::init( "loader" ), false );
